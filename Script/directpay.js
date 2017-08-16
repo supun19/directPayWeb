ca33abde60e7aedb4f48affb9d216e2788d8f435
supun19 <supun12.@cse.mrt.ac.lk>
2017-08-16 12:06:38 +0530

    moll

--- Script/directpay.js
+++ Script/directpay.js
@@ -6,7 +6,7 @@ redirect_to_directpay = function(){
 	//window.location.href = "http://192.168.8.103:8000/";
 	//window.location.replace("http://192.168.8.103:8000/");
 	var  transactionId = "123456";
-	var data ={"id":"123456","merchantId":"c1ec6fad-6541-4da1-8939-2ec7bff0491c","amount":"1200.00"};
+	var data ={"id":"123456","merchantId":"2800c41b-84ca-4b9d-8c04-87a4664fe87d","amount":"1200.00"};
 
 	
 /*	$.ajax({
