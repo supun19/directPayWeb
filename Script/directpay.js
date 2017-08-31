04833dcd9a9329470f93afa6c8ff9afe4aeb7531
supun19 <supun12.@cse.mrt.ac.lk>
2017-08-31 10:43:29 +0530

    Change Id

--- Script/directpay.js
+++ Script/directpay.js
@@ -6,7 +6,7 @@ redirect_to_directpay = function(){
 	//window.location.href = "http://192.168.8.103:8000/";
 	//window.location.replace("http://192.168.8.103:8000/");
 	var  transactionId = "123456";
-	var data ={"id":"123456","merchantId":"2800c41b-84ca-4b9d-8c04-87a4664fe87d","amount":"1200.00"};
+	var data ={"id":"123456","merchantId":"d86e8a28-bcb7-47fd-b0e5-2c7cf2d26576","amount":"1200.00"};
 
 	
 /*	$.ajax({
