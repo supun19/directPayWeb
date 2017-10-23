aabe8f06e9b6a064d012e76ccfb458a0006acaed
supun19 <supun.12@cse.mrt.ac.lk>
2017-10-23 15:37:09 +0530

    Redirect Url

--- Script/directpay.js
+++ Script/directpay.js
@@ -6,7 +6,7 @@ redirect_to_directpay = function(){
 	//window.location.href = "http://192.168.8.103:8000/";
 	//window.location.replace("http://192.168.8.103:8000/");
 	var  transactionId = "123456";
-	var data ={"id":"123456","merchantId":"d86e8a28-bcb7-47fd-b0e5-2c7cf2d26576","amount":"1200.00"};
+	var data ={"id":"123456","merchantId":"62fdc4ec-34a9-4fbd-bd88-a2260d75b1f1","amount":"1200.00","rUrl":"localhost:5002"};
 
 	
 /*	$.ajax({
@@ -25,8 +25,8 @@ redirect_to_directpay = function(){
         alert(errMsg);
     }
 });*/
-	
-	window.location.href='http://13.58.144.197/default/web/purchase?id='+data.id+'&merchantId='+data.merchantId+'&amount='+data.amount;
+	console.log(encodeURIComponent(data.rUrl));
+	window.location.href='http://13.58.144.197/default/web/purchase?id='+data.id+'&merchantId='+data.merchantId+'&amount='+data.amount+'&rUrl='+encodeURIComponent(data.rUrl);
 	
 	
 	
